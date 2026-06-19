d7deaff14a7a37316edb2700283312cfb3e587ac
4athakur <amitthakuramitthakur630@gmail.com>
2026-06-19 09:11:56 +0530

    add config file

---
+++ config.py
@@ -0,0 +1,11 @@
+API_K = "6i5gmMA1mPuMwsYkD5txDsshw4XYHJ"
+API_S = "F40KLO5rg3SJOq6D4f3OdEdcoiZo9M9xUx53MsXse2M19xUK4Ta67JDSYp3F"
+path = "/v2/funding_rates"
+
+
+
+BASE_URL = "https://api.india.delta.exchange"
+path_wallet_balance="/v2/wallet/balances"
+
+coinddcx_api_key="cab820cb110f98e3299deeff872d5deafecc92f7f95e39a9"
+coinddcx_api_secret="a7c89815abff0bb3c88386d11cd7c093ffe8945f1155e3cdb2d3075ae75412e8"
